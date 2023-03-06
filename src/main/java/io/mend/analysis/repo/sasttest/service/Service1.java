3e0382c768348f8e3f7af15b9f7cfbad36d048b2
GuyHudara <hudara.guy@gmail.com>
2023-03-06 12:26:01 +0200

    3.0.0

--- src/main/java/io/mend/analysis/repo/sasttest/service/Service1.java
+++ src/main/java/io/mend/analysis/repo/sasttest/service/Service1.java
@@ -2,6 +2,7 @@ package io.mend.analysis.repo.sasttest.service;
 
 import io.mend.analysis.repo.sasttest.model.Model1;
 import io.mend.analysis.repo.sasttest.model.Model2;
+import io.mend.analysis.repo.sasttest.model.Model3;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +11,7 @@ public class Service1 {
 
   @Autowired private Model1 model1;
   @Autowired private Model2 model2;
+  @Autowired private Model3 model3;
 
   public String logInfoUserInput(String userInput) {
 
@@ -17,6 +19,6 @@ public class Service1 {
   }
 
   public String logDebugUserInput(String userInput) {
-    return model2.logDebugUserInput(userInput);
+    return model3.logDebugUserInput(userInput);
   }
 }
