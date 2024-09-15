c71677db06730c6bdf34e1c06e2f8cfeeaca8c49
[V2]GHActions <[V2]GHActions>
2024-09-15 14:13:25 +0300

    test

--- jssrc/service/service1.js
+++ jssrc/service/service1.js
@@ -1,7 +1,12 @@
 const model1 = require('./../model/model1');
 const model2 = require('./../model/model2');
 
+let fruits = ["Apple", "Banana", "Orange"];
+
 exports.logInfoUserInput = (userInput) => {
+    // 2. Accessing elements
+    console.log("First fruit:", fruits[0]); // Output: Apple
+    console.log("Second fruit:", fruits[1]); // Output: Banana
     return model1.logInfoUserInput(userInput, 12);
 };
 
